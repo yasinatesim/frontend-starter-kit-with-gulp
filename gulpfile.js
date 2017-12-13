775a7f66a446608a1d94ebe8e62bce8813490828
Yasin ATEŞ <32565521+yasinatesim@users.noreply.github.com>
2017-12-13 20:13:31 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,74 +9,77 @@
  * -----------------------------------------------------------------------------
  */
 
- /* ================= Gulp ==================== */
- const gulp         = require('gulp'),
- gutil        = require('gulp-util'),
+/* ================= Gulp ==================== */
+const gulp = require('gulp'),
+    gutil = require('gulp-util'),
 
- /* ================= Pug ==================== */
- pug          = require('gulp-pug'),
- prettify     = require('gulp-prettify'),
+    /* ================= Pug ==================== */
+    pug = require('gulp-pug'),
+    prettify = require('gulp-prettify'),
 
- /* ================= Sass ==================== */
- sass         = require('gulp-sass'),
- autoprefixer = require('gulp-autoprefixer'),
- cssmin 	   = require('gulp-cssmin'),
+    /* ================= Sass ==================== */
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin = require('gulp-cssmin'),
 
- /* ================= Babel ==================== */
- babel        = require('gulp-babel'),
- uglify       = require('gulp-uglify'),
+    /* ================= Babel ==================== */
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify'),
 
- /* ================= Image ==================== */
- imagemin     = require('gulp-imagemin'),
+    /* ================= Image ==================== */
+    imagemin = require('gulp-imagemin'),
 
- /* ================= File Name & Includes ==================== */
- rename       = require('gulp-rename'),
- include      = require('gulp-include'),
+    /* ================= File Name & Includes ==================== */
+    rename = require('gulp-rename'),
+    include = require('gulp-include'),
 
- /* =================  Eror Reporting  ==================== */
- plumber      = require('gulp-plumber'),
+    /* =================  Eror Reporting  ==================== */
+    plumber = require('gulp-plumber'),
 
- /* ================= Compaile & Server ==================== */
- watch        = require('gulp-watch'),
- del 		  = require('del'),
- sequence 	  = require('run-sequence'),
- bs           = require('browser-sync'),
+    /* ================= Compaile & Server ==================== */
+    watch = require('gulp-watch'),
+    del = require('del'),
+    sequence = require('run-sequence'),
+    bs = require('browser-sync'),
 
-/**
- * Output Css & Js File Name and Set Paths
- * -----------------------------------------------------------------------------
- */
+    /**
+     * Output Css & Js File Name and Set Paths
+     * -----------------------------------------------------------------------------
+     */
 
-ThemeName = 'theme', // Output CSS and Javascript File Name
-path      = {
-	developmentDir: 'resources',
-	productionDir: 	ThemeName.charAt(0).toUpperCase() + ThemeName.slice(1) + ' HTML'
-};
+    ThemeName = 'theme', // Output CSS and Javascript File Name
+    path = {
+        developmentDir: 'resources',
+        productionDir: ThemeName.charAt(0).toUpperCase() + ThemeName.slice(1) + ' HTML'
+    };
 
 /**
  * Catch stream errors
  * -----------------------------------------------------------------------------
  */
 
- const gulpSrc = gulp.src;
+const gulpSrc = gulp.src;
 
- gulp.src = function onError(...args) {
- 	return gulpSrc
- 	.apply(gulp, args)
-    // Catch errors
-    .pipe(plumber(function onError(error) {
-    	gutil.log(gutil.colors.bgRed("Error (" + error.plugin + "):" + error.message));
-    	this.emit('end');
-    }));
-};
+gulp.src = function onError(...args
+)
+{
+    return gulpSrc
+        .apply(gulp, args)
+        // Catch errors
+        .pipe(plumber(function onError(error) {
+            gutil.log(gutil.colors.bgRed("Error (" + error.plugin + "):" + error.message));
+            this.emit('end');
+        }));
+}
+;
 
 /**
  * Delete the productionDir directory
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('clean', function () {
-  return del(path.productionDir);
+gulp.task('clean', function () {
+    return del(path.productionDir);
 });
 
 /**
@@ -84,24 +87,24 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('pug', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/pug/*.pug')
-  // Compile Pug
-  .pipe(pug(
-  {
-  	pretty: true
-  }
-  ))
-  // HTML Beautify
-  .pipe(prettify({
-  	indent_size: 4,
-  	unformatted: ['pre', 'code'],
-  	preserve_newlines: true
-  }))
-  // Save files
-  .pipe(gulp.dest(path.productionDir));
+gulp.task('pug', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/pug/*.pug')
+        // Compile Pug
+        .pipe(pug(
+            {
+                pretty: true
+            }
+        ))
+        // HTML Beautify
+        .pipe(prettify({
+            indent_size: 4,
+            unformatted: ['pre', 'code'],
+            preserve_newlines: true
+        }))
+        // Save files
+        .pipe(gulp.dest(path.productionDir));
 });
 
 /**
@@ -109,35 +112,35 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('sass', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/sass/styles.scss')
-  // Compile Sass
-  .pipe(sass(
-  {
-  	outputStyle: 'expanded'
-  }
-  ))
-  // Add vendor prefixes
-  .pipe(autoprefixer(
-  {
-  	browsers: ['last 4 version'],
-  	cascade: false
-  }
-  ))
-  // File Name
-  .pipe(rename(ThemeName + '-styles.css'))
-  // Save unminified file
-  .pipe(gulp.dest(path.productionDir + '/assets/css'))
-  // Optimize and minify
-  .pipe(cssmin())
-  // Append suffix
-  .pipe(rename({
-  	suffix: '.min'
-  }))
-  // Save minified file
-  .pipe(gulp.dest(path.productionDir + '/assets/css'));
+gulp.task('sass', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/sass/styles.scss')
+        // Compile Sass
+        .pipe(sass(
+            {
+                outputStyle: 'expanded'
+            }
+        ))
+        // Add vendor prefixes
+        .pipe(autoprefixer(
+            {
+                browsers: ['last 4 version'],
+                cascade: false
+            }
+        ))
+        // File Name
+        .pipe(rename(ThemeName + '-styles.css'))
+        // Save unminified file
+        .pipe(gulp.dest(path.productionDir + '/assets/css'))
+        // Optimize and minify
+        .pipe(cssmin())
+        // Append suffix
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        // Save minified file
+        .pipe(gulp.dest(path.productionDir + '/assets/css'));
 });
 
 /**
@@ -145,33 +148,33 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('themes', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/sass/themes/*.scss')
-  // Compile Sass
-  .pipe(sass(
-  {
-  	outputStyle: 'expanded'
-  }
-  ))
-  // Add vendor prefixes
-  .pipe(autoprefixer(
-  {
-  	browsers: ['last 4 version'],
-  	cascade: false
-  }
-  ))
-  // Save unminified file
-  .pipe(gulp.dest(path.productionDir + '/assets/css/colors'))
-  // Optimize and minify
-  .pipe(cssmin())
-  // Append suffix
-  .pipe(rename({
-  	suffix: '.min'
-  }))
-  // Save minified file
-  .pipe(gulp.dest(path.productionDir + '/assets/css/colors'));
+gulp.task('themes', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/sass/themes/*.scss')
+        // Compile Sass
+        .pipe(sass(
+            {
+                outputStyle: 'expanded'
+            }
+        ))
+        // Add vendor prefixes
+        .pipe(autoprefixer(
+            {
+                browsers: ['last 4 version'],
+                cascade: false
+            }
+        ))
+        // Save unminified file
+        .pipe(gulp.dest(path.productionDir + '/assets/css/colors'))
+        // Optimize and minify
+        .pipe(cssmin())
+        // Append suffix
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        // Save minified file
+        .pipe(gulp.dest(path.productionDir + '/assets/css/colors'));
 });
 
 /**
@@ -179,30 +182,30 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('js', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/babel/scripts.js')
-  // Concatenate includes
-  .pipe(include())
-  // Transpile
-  .pipe(babel(
-  {
-          presets: [['env', {loose: true, modules: false}]] // 'use-strict' deleted
-      }
-      ))
-  // File Name
-  .pipe(rename(ThemeName + '-scripts.js'))
-  // Save unminified file
-  .pipe(gulp.dest(path.productionDir + '/assets/js'))
-  // Optimize and minify
-  .pipe(uglify())
-  // Append suffix
-  .pipe(rename({
-  	suffix: '.min'
-  }))
-  // Save minified file
-  .pipe(gulp.dest(path.productionDir + '/assets/js'));
+gulp.task('js', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/babel/scripts.js')
+        // Concatenate includes
+        .pipe(include())
+        // Transpile
+        .pipe(babel(
+            {
+                presets: [['env', {loose: true, modules: false}]] // 'use-strict' deleted
+            }
+        ))
+        // File Name
+        .pipe(rename(ThemeName + '-scripts.js'))
+        // Save unminified file
+        .pipe(gulp.dest(path.productionDir + '/assets/js'))
+        // Optimize and minify
+        .pipe(uglify())
+        // Append suffix
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        // Save minified file
+        .pipe(gulp.dest(path.productionDir + '/assets/js'));
 });
 
 /**
@@ -210,14 +213,14 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('images', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/images/**/*')
-  // ImageMin
-  .pipe(imagemin())
-  // Save files
-  .pipe(gulp.dest(path.productionDir + '/assets/img'));
+gulp.task('images', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/images/**/*')
+        // ImageMin
+        .pipe(imagemin())
+        // Save files
+        .pipe(gulp.dest(path.productionDir + '/assets/img'));
 });
 
 /**
@@ -225,12 +228,12 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('vendors', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/vendors/**/*')
-  // Save files
-  .pipe(gulp.dest(path.productionDir + '/assets/vendors'));
+gulp.task('vendors', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/vendors/**/*')
+        // Save files
+        .pipe(gulp.dest(path.productionDir + '/assets/vendors'));
 });
 
 /**
@@ -238,12 +241,12 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('fonts', function () {
- 	return gulp
-  // Select files
-  .src(path.developmentDir + '/fonts/*')
-  // Save files
-  .pipe(gulp.dest(path.productionDir + '/assets/fonts'));
+gulp.task('fonts', function () {
+    return gulp
+    // Select files
+        .src(path.developmentDir + '/fonts/*')
+        // Save files
+        .pipe(gulp.dest(path.productionDir + '/assets/fonts'));
 });
 
 /**
@@ -251,48 +254,48 @@ path      = {
  * -----------------------------------------------------------------------------
  */
 
- gulp.task('server', function () {
+gulp.task('server', function () {
 
 // Create and initialize local server
-bs.create();
-bs.init({
-	notify: false,
-	server: './' + path.productionDir,
-	open: 'local',
-	ui: false
-});
+    bs.create();
+    bs.init({
+        notify: false,
+        server: './' + path.productionDir,
+        open: 'local',
+        ui: false
+    });
 
-  // Watch for build changes and reload browser
-  bs.watch(path.productionDir + '/**/*').on('change', bs.reload);
+    // Watch for build changes and reload browser
+    bs.watch(path.productionDir + '/**/*').on('change', bs.reload);
 
-  // Watch for source changes and execute associated tasks
-  watch('./'+ path.developmentDir + '/pug/**/*.pug', function () {
-  	gulp.start('pug');
-  });
+    // Watch for source changes and execute associated tasks
+    watch('./' + path.developmentDir + '/pug/**/*.pug', function () {
+        gulp.start('pug');
+    });
 
-  watch(['./'+ path.developmentDir + '/sass/**/*.scss','!./'+ path.developmentDir + '/sass/themes/*.scss'], function () {
-  	gulp.start('sass');
-  });
+    watch(['./' + path.developmentDir + '/sass/**/*.scss', '!./' + path.developmentDir + '/sass/themes/*.scss'], function () {
+        gulp.start('sass');
+    });
 
-  watch('./'+ path.developmentDir + '/sass/themes/*.scss', function () {
-  	gulp.start('themes');
-  });
+    watch('./' + path.developmentDir + '/sass/themes/*.scss', function () {
+        gulp.start('themes');
+    });
 
-  watch('./'+ path.developmentDir + '/babel/**/*.js', function () {
-  	gulp.start('js');
-  });
+    watch('./' + path.developmentDir + '/babel/**/*.js', function () {
+        gulp.start('js');
+    });
 
-  watch('./'+ path.developmentDir + '/images/**/*', function () {
-  	gulp.start('images');
-  });
+    watch('./' + path.developmentDir + '/images/**/*', function () {
+        gulp.start('images');
+    });
 
-  watch('./'+ path.developmentDir + '/vendors/**/*', function () {
-  	gulp.start('vendors');
-  });
+    watch('./' + path.developmentDir + '/vendors/**/*', function () {
+        gulp.start('vendors');
+    });
 
-  watch('./'+ path.developmentDir + '/fonts/*', function () {
-  	gulp.start('fonts');
-  });
+    watch('./' + path.developmentDir + '/fonts/*', function () {
+        gulp.start('fonts');
+    });
 
 });
 
@@ -302,5 +305,5 @@ bs.init({
  */
 
 gulp.task('default', function (callback) {
-  return sequence(['clean'], ['pug'], ['sass'], ['themes'], ['js'], ['images'], ['vendors'], ['fonts'], ['server'], callback);
+    return sequence(['clean'], ['pug'], ['sass'], ['themes'], ['js'], ['images'], ['vendors'], ['fonts'], ['server'], callback);
 });
