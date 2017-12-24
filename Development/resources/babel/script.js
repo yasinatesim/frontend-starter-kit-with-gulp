40a3c6845db7a49029ba224bb3848826146f93c8
Yasin ATEŞ <32565521+yasinatesim@users.noreply.github.com>
2017-12-24 09:44:49 +0300

    Update script.js

--- Development/resources/babel/script.js
+++ Development/resources/babel/script.js
@@ -1,3 +1,20 @@
+/*!
+
+	Template;
+		* Name	   	  : ThemeName
+		* Created Date    : date
+		* Version         : 0.0.1
+
+	Author;
+		* Gmail     : yasinatesim@gmail.com
+		* Web Site  : http://www.yasinates.com
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Copyright (c) 2017 - yasinatesim - https://sitename.net/user/yasinatesim
+============================================================================================
+
+*/
+
 $(document).ready(function(){
   'use strict';
   
